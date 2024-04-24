c3d3c61275693fb1e0cb9b01197b0ddd92a479ae
Tony <122675474+Noidan@users.noreply.github.com>
2024-04-24 16:52:11 +0300

    Cart component

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -6,16 +6,16 @@ import { ROUTES } from './../../utils/routes';
 import SingleProduct from '../Products/SingleProduct';
 import Home from '../Home/Home'
 import Profile from '../Profile/Profile';
-//import SingleCategory from '../Categories/SingleCategory';
-//import Cart from '../Cart/Cart';
+import SingleCategory from '../Categories/SingleCategory';
+import Cart from '../Cart/Cart';
 
 const AppRoutes = () => (
   <Routes>
     <Route index element={<Home />} />
     <Route path={ROUTES.PRODUCT} element={<SingleProduct />} />
     <Route path={ROUTES.PROFILE} element={<Profile />} />
-    {/* <Route path={ROUTES.CATEGORY} element={<SingleCategory />} />
-    <Route path={ROUTES.CART} element={<Cart />} /> */}
+    <Route path={ROUTES.CATEGORY} element={<SingleCategory />} />
+    <Route path={ROUTES.CART} element={<Cart />} />
   </Routes>
 );
 
